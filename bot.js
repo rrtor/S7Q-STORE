0cf893f736b94ef81fc7ab544daa25a189b2e3be
rrtor <41769454+rrtor@users.noreply.github.com>
2018-09-04 01:22:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,3 +10,62 @@ client.on('message', message => {
         message.reply('pong');
       }
 });
+
+ client.on('message', async rokz => {
+    if(rokz.content.startsWith(prefix + "تقديم")) {
+      let lang = '';
+      let time = '';
+      let expe = '';
+      let fillter = m => m.author.id === rokz.author.id
+      await rokz.channel.send("ما لغتك ؟").then(e => {
+     rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+     .then(co => {
+       lang = co.first().content;
+        co.first().delete();
+
+       e.edit(`مدة في اللغه
+[${lang}]`)
+       rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+       .then(col => {
+         time = col.first().content;
+          col.first().delete();
+
+            e.edit(`خبرتك ؟
+[${time}]
+[${lang}]`)
+            rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+            .then(coll => {
+              expe = coll.first().content;
+               coll.first().delete();
+
+               e.edit(`جاري تقديمك...
+[${expe}]
+[${time}]
+[${lang}]`)
+              let rokzz = rokz.guild.channels.find("name", "تقديم")
+              setTimeout(() => {
+                e.edit("تم التقديم")
+              }, 3000)
+              rokzz.send(`
+                تقديم من ${rokz.author}
+
+  اللغه:
+  \`${lang}\`
+  المده:
+  \`${time}\`
+  الخبره:
+  \`${expe}\`
+                `).then(rokzzz => {
+                  rokzzz.react("✅")
+                  rokzzz.react("❌")
+                })
+            })
+       })
+     })
+   })
+    }
+  })
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
